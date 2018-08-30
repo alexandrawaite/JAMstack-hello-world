96599749dfea98587008673a3dacf56dbd3df748
alexandrawaite <alexandrawaite@gmail.com>
2018-08-30 14:01:36 -0700

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi all!</h1>
+    <p>My name is Alex Waite and I'm a software developer who wants to use technology to bring opportunities to underserved communities.</p>
+    <p>I'm excited about the JAMStack Hackathon and can't wait to build something great!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
